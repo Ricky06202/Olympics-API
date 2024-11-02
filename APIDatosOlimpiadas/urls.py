c64d808a3155dbf61451c2f2aa8057e0ed3d1ba7
Azner01 <azneraparicio@hotmail.com>
2024-11-01 19:23:50 -0500

    Token de usuario y GET con autenticación

--- APIDatosOlimpiadas/urls.py
+++ APIDatosOlimpiadas/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from . import views
 
@@ -7,4 +7,8 @@ router.register(r"Olympics", views.OlympicsViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    re_path('login', views.login),
+    re_path('register', views.register),
+    re_path('profile', views.profile),
+    re_path('olympics_data', views.olympics_data),
 ]
--- OlympicsDB/settings.py
+++ OlympicsDB/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "APIDatosOlimpiadas",
     "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
 ]
 
